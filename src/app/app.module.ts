c1d7d7b1b69046ed417c7b84e29571f649fad160
AndrewJBateman <gomezbateman@yahoo.com>
2023-05-01 18:58:39 +0200

    styles: update to new ng material version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -21,17 +21,24 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { WeatherReportComponent } from './weather-report/weather-report.component';
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from './app-routing.module';
 import { SvgSunComponent } from './components/svg-sun/svg-sun.component';
 import { SvgMoonComponent } from './components/svg-moon/svg-moon.component';
 
 @NgModule({
-  declarations: [AppComponent, SampleDialogComponent, WeatherReportComponent, SvgSunComponent, SvgMoonComponent],
+  declarations: [
+    AppComponent,
+    SampleDialogComponent,
+    WeatherReportComponent,
+    SvgSunComponent,
+    SvgMoonComponent,
+  ],
   imports: [
     AppRoutingModule,
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
     MatSelectModule,
     MatProgressBarModule,
@@ -49,5 +56,6 @@ import { SvgMoonComponent } from './components/svg-moon/svg-moon.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
